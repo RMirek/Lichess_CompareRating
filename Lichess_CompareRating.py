caccecdc06bbdf7cea60c7d20c1acc06eb87c8db
RMirek <43520493+RMirek@users.noreply.github.com>
2021-05-11 22:02:09 +0200

    Update Lichess_CompareRating.py

--- Lichess_CompareRating.py
+++ Lichess_CompareRating.py
@@ -24,9 +24,9 @@ for i, user in enumerate(users_list):
         if game['name'] == GAME_TYPE:
             SAVE_NAME = '_'.join(users_list) + '_' + GAME_TYPE
             points = np.array(game['points'])
-            dates = [datetime.datetime(x[0], x[1]+1, x[2]) for x in points[:,:3]]
-            plt.plot(dates, points[:,3], label=user, linewidth=2)
-            time.sleep(1)
+            if len(points)>0:
+                dates = [datetime.datetime(x[0], x[1]+1, x[2]) for x in points[:,:3]]
+                plt.plot(dates, points[:,3], label=user, linewidth=2)
     
 ax.set_ylabel(GAME_TYPE + ' rating',fontsize=20)
 ax.set_xlabel('Date',fontsize=20)   
