fda7fe79bcdddf8305738f6953b20a3c6567d079
RMirek <43520493+RMirek@users.noreply.github.com>
2021-09-12 19:24:18 +0200

    Update Lichess_CompareRating.py
    Improved readability of the code, added possibility to plot more than one variant on one figure.

--- Lichess_CompareRating.py
+++ Lichess_CompareRating.py
@@ -6,37 +6,59 @@ import numpy as np
 import datetime
 import matplotlib.dates as mdates
 
-def get_user_points(user):
+def getUserPoints(user):
     user_history = urllib.request.urlopen(
         "http://en.lichess.org/api/user/" + user + '/rating-history').read()
     return json.loads(user_history)
 
-users_list = ['rwmmir', 'Thibault'] # list of users, you can put as many as you want
+def createFigure(variants, ncols=2):
+    nrows = int(np.ceil(len(variants)/ncols))    
+    fig, ax = plt.subplots(nrows, ncols, figsize=(8*nrows, 5*ncols), facecolor='w', edgecolor='k')
+    return fig, ax
 
-GAME_TYPE = 'Blitz' #Bullet, Blitz, Rapid, Classical...
+def get_nrow_ncol(number):
+    nrow = int(np.floor(number/2))
+    ncol = int(np.ceil(number/2)-1)
+    return nrow, ncol
 
-fig, ax = plt.subplots(1,1, figsize=(8, 6), facecolor='w', edgecolor='k')
-
-for i, user in enumerate(users_list):
-    print('Processing user: ', user)
-    user_points = get_user_points(user)
-    for game in user_points: 
-        if game['name'] == GAME_TYPE:
-            SAVE_NAME = '_'.join(users_list) + '_' + GAME_TYPE
-            points = np.array(game['points'])
-            if len(points)>0:
-                dates = [datetime.datetime(x[0], x[1]+1, x[2]) for x in points[:,:3]]
-                plt.plot(dates, points[:,3], label=user, linewidth=2)
+def formatTicks(nrow, ncol):
+    ax[nrow, ncol].format_xdata = mdates.DateFormatter('%Y')    
+    fmt_month = mdates.MonthLocator()
+    ax[nrow, ncol].xaxis.set_minor_locator(fmt_month)
+    ax[nrow, ncol].set_ylabel(variant + ' rating',fontsize=20)
+    ax[nrow, ncol].set_xlabel('Date',fontsize=20)   
     
-ax.set_ylabel(GAME_TYPE + ' rating',fontsize=20)
-ax.set_xlabel('Date',fontsize=20)   
-fig.autofmt_xdate()
-ax.fmt_xdata = mdates.DateFormatter('%Y-%m-%d')
-plt.legend(fontsize=20)
-ax.tick_params(axis='both', direction = "in", which='major', labelsize = 20,
-    right = True, top = True, left = True)    
+    ax[nrow, ncol].legend(fontsize=20)
+    ax[nrow, ncol].tick_params(axis='both', direction = "in", which='major', labelsize = 20,
+        right = True, top = True, left = True)    
+    plt.setp(ax[nrow, ncol].get_xticklabels(), rotation=30, horizontalalignment='right')
+    
+users_list = ['rwmmir', 'Thibault', 'sgis'] # list of users, you can put as many as you want
+variants = ['Bullet', 'Blitz', 'Rapid', 'Classical'] # variants, you can put as many as you want
+
+fig, ax = createFigure(variants)
+
+for i, variant in enumerate(variants):
+    nrow, ncol = get_nrow_ncol(i)
+    for user in users_list:
+        userPoints = getUserPoints(user)
+        for game in userPoints: 
+            if game['name'] == variant:
+                points = np.array(game['points'])
+                if len(points)>0:
+                    dates = [datetime.datetime(x[0], x[1]+1, x[2]) for x in points[:,:3]]
+                    ax[nrow, ncol].plot(dates, points[:,3], label=user, linewidth=2)
+                   
+    formatTicks(nrow, ncol)                
+    
+if len(variants)%2==1:
+    ax[nrow][ncol+1].set_visible(False)     
+
+fig.tight_layout()        
 plt.show()
-# fig.savefig(SAVE_NAME, bbox_inches='tight', dpi=100)
+
+# save_name = '_'.join(users_list) + '_'.join(variants)
+# fig.savefig(save_name, bbox_inches='tight', dpi=100)
 # plt.cla()
 # plt.clf()
 # plt.close('all')
